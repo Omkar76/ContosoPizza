a249ae85670a46db374355eb4aeabe478592f26d
Onkar Jadhav <omjadhav2610@gmail.com>
2022-07-27 12:56:38 +0530

    Add project files.

---
+++ Models/Pizza.cs
@@ -0,0 +1,8 @@
+﻿namespace ContosoPizza.Models;
+public class Pizza
+{
+
+    public int Id { get; set; }
+    public string Name { get; set; } = String.Empty;
+    public bool IsGlutenFree { get; set; }
+}
